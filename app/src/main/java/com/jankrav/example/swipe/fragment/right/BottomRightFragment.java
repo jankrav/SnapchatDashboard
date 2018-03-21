240fef25ed51fe9663454014fe369e2b43f52316
Ivan Kravchuk <ivankravchuk10@gmail.com>
2018-03-21 18:35:59 +0200

    added simple vertical pager

---
+++ app/src/main/java/com/jankrav/example/swipe/fragment/right/BottomRightFragment.java
@@ -0,0 +1,31 @@
+package com.jankrav.example.swipe.fragment.right;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jankrav.example.swipe.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BottomRightFragment extends Fragment {
+
+
+    public BottomRightFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_right, container, false);
+    }
+
+}
